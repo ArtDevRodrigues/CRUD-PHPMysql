d88454caa79e489ad4764260a3f5559cd068a3a3
Arthur Holanda Rodrigues <79876110+ArtDevRodrigues@users.noreply.github.com>
2021-09-30 20:44:01 -0300

    update-crud-note-complete

--- index.php
+++ index.php
@@ -11,128 +11,93 @@
         include("connection.php");
         
         $userId = $_POST['id'];
-        //Funcoes da tabela Notas
-        /*
-        idFunction
-        1-create
-        2-update
-        3-delete
-         */
-        if ($_POST['idFunction'] == 1) {
-            $title = $_POST['title'];
-            $desc = $_POST['desc'];
-            $query = "insert into note(title_,desc_,is_finished,user_id) value('{$title}','{$desc}',false,{$userId});";
-            mysqli_query($mysqli, $query);
+        $userPass = $_POST['pass'];
+        //echo "$userId";
+        $canIn = false;
         
-        }elseif ($_POST['idFunction'] == 2) {
-            $taskId = $_POST['idTask'];
-            $title = $_POST['title'];
-            $desc = $_POST['desc'];
-            $isFin = boolval($_POST['isFin']);    
-            $query = "update note set title_='$title',desc_='$desc',is_finished=$isFin where id_ = $taskId";
-            mysqli_query($mysqli, $query);
-        
-        }elseif($_POST['idFunction'] == 3) {
-            $taskId = $_POST['idTask'];
-            $query = "delete from note where id_ = $taskId";
-            mysqli_query($mysqli, $query);
-        }
+        include("taskDAO.php");
 
+        // User
+        $datasUser = null;
+        $resultUser = null;
+        if ($userId != null) {
+            $sql1 = "select * from user_ where user_id = $userId";
+            $resultUser = $mysqli->query($sql1);
+            $datasUser = $resultUser->fetch_assoc();
+            
+        }
+        $is_Users_ok = false;
+        
+        // Check user
+        if ($userId == $datasUser['user_id'] & $userPass == $datasUser['user_pass']) {
+            $canIn = true;
+        };
+
+        if ($resultUser != null & $canIn) {$is_Users_ok = true;}
+        else {$is_Users_ok = false; }
 
         //Notes
         $is_Notes_ok = false;
         $sql2 = "select * from note where user_id = $userId";
         $resultNotes = $mysqli->query($sql2);
-        if ($resultNotes != null) {$is_Notes_ok = true;}
+        if ($resultNotes != null & $canIn) {$is_Notes_ok = true;}
         else {$is_Notes_ok = false; }
 
-        // User
-        $sql1 = "select * from user_ where user_id = $userId";
-        $is_Users_ok = false;
-        $datasUser = null;
-        $resultUser = $mysqli->query($sql1);
-        if ($resultUser != null) {$is_Users_ok = true;$datasUser = $resultUser->fetch_assoc();}
-        else {$is_Users_ok = false; }
-
-        
     ?>
 </head>
 <body>
 <!-- SingIn-->
 <div class="modal fade" id="SingIn" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <form action="index.php" method="POST">
-        <div class="modal-dialog">
-            <div class="modal-content">
+    <div class="modal-dialog">
+        <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Entrar</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <form action="index.php" method="POST">
+                <div class="modal-body">
 
-            <input type="text" placeholder="Indentificador" name="id">
-            <input type="text" placeholder="Senha" name="pass">
+                <input class="form-control" type="text" placeholder="Indentificador" name="id">
+                <input class="form-control" type="password" size="20" placeholder="Senha" name="pass">
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                <button class="btn btn-primary">Entrar</button>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button class="btn btn-primary">Entrar</button>
+                </div>
+        </form>
             </div>
         </div>
-    </form>
 </div>
 <!-- SingUp-->
 <div class="modal fade" id="SingUp" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <form action="index.php" method="POST">
-        <div class="modal-dialog">
-            <div class="modal-content">
+    <div class="modal-dialog">
+        <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Cadastro</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <form action="index.php" method="POST">
+                <div class="modal-body">
 
-            <input type="text" placeholder="Indentificador" name="id">
-            <input type="text" placeholder="Nome" name="name">
-            <input type="text" placeholder="Senha" name="pass">
+                <input class="form-control" type="text" placeholder="Indentificador" name="id">
+                <input class="form-control" type="text" placeholder="Nome" name="name">
+                <input class="form-control" type="password" size="20" placeholder="Senha" name="pass">
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                <button class="btn btn-primary">Cadastar</button>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button class="btn btn-primary">Cadastar</button>
+                </div>
+            </form>
             </div>
         </div>
-    </form>
 </div>
-<!-- SingUp-->
-<div class="modal fade" id="SingUp" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <form action="index.php" method="POST">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Cadastro</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
 
-            <input type="text" placeholder="Indentificador" name="id">
-            <input type="text" placeholder="Nome" name="name">
-            <input type="password" size="20" placeholder="Senha" name="pass">
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                <button class="btn btn-primary">Cadastar</button>
-            </div>
-            </div>
-        </div>
-    </form>
-</div>
 <!-- CadTask-->
 <div class="modal fade" id="CadTask" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-            <form action="cadTask.php" method="POST">
+            <form action="index.php" method="POST">
             <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Nova Nota</h5>
@@ -140,9 +105,12 @@
             </div>
             <div class="modal-body">
 
-            <input type="text" placeholder="Titulo" name="title">
-            <textarea name='desc' id='text' cols='20' rows='10'></textarea>
-            <?php echo "<input type='hidden' name='id' value='$userId'>"; ?>
+            <input class="form-control" type="text" placeholder="Titulo" name="title">
+            <textarea class="form-control" name='desc' id='text' cols='50' rows='10'></textarea>
+            <?php 
+            echo "<input type='hidden' name='id' value='$userId'>"; 
+            echo "<input type='hidden' name='pass' value='$userPass'>"; 
+            echo "<input type='hidden' name='idFunction' value='1'><br>";?>
             
             </div>
             <div class="modal-footer">
@@ -156,20 +124,19 @@
 
 <div class="container">
     <?php if ($is_Users_ok) {?>
-        
-        <div class="row">
-            <div class="col"></div>
-            <div class="col col-11">
-                <h2>Lista de tarefas Pessoal Usuario(a) <?php echo $datasUser['user_name']; ?></h2>
+        <div class="container">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col-8"><h2>Lista de tarefas Pessoal Usuario(a): <?php echo $datasUser['user_name']; ?></h2></div>
+                <div class="col"><form class="text-center" action="index.php" method="POST"><input  type='hidden' name='id' value=''><input type='hidden' name='pass' value=''><button class="btn btn-primary">  Sair   </button></form></div>
             </div>
-            <div class="col"><form action="index.php" method="POST"><input type='hidden' name='id' value=''><button class="btn btn-primary">Sair</button></form></div>
         </div>
     <?php }else{ ?>
 
         <div class="row">
             <div class="col"></div>
-            <div class="col">
-                <h3>Entre em sua conta ou Crie uma</h3>
+            <div class="col-8">
+                <h3 class="text-center">Entre em sua conta ou Crie uma</h3>
             </div>
             <div class="col"></div>
         </div>
@@ -180,40 +147,42 @@
         <?php if($is_Notes_ok == false){ ?>
 
             <div class="row">
-                <div class="col-10"></div>
-                <div class="col"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#SingIn">Entrar</button></div>
+                <div class="col"></div>
+                <div class="col col-md-auto"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#SingIn">Entrar</button></div>
                
-                <div class="col"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#SingUp">Cadastrar</button></div>
+                <div class="col col-md-auto"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#SingUp">Cadastrar</button></div>
             </div>
-            
         <?php }else{ ?>
             <div class="row">
-                <div class="col-10"></div>
-                <div class="col"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#CadTask">Adicionar uma nova tarefa</button></div>
+                <div class="col-md-8"></div>
+                <div class="col-6 col-md-4"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#CadTask">Adicionar uma nova tarefa</button></div>
             </div>
             <br>
+            <!-- mostrando os dados -->
             <?php while ($dado = $resultNotes->fetch_assoc()){ ?>
-                <?php // for($i = 0; $i <= 6; $i++) { ?>
-            <div class="col col-lg-2 border border-3 p-3 mb-2 bg-secondary text-white">
+                
+            <div class="col col-lg-4 border border-3 p-2 mb-3 bg-secondary">
             <?php 
                 $title = $dado['title_'];
                 $desc = $dado['desc_'];
                 $id = $dado['id_'];
             ?>
-            <button type="button" data-bs-toggle="modal" data-bs-target="#UpTask<?php echo $id;?>">
-            <div class="canvas canvas-start" aria-labelledby="offcanvasLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasLabel"><?php echo "<h6>Titulo: $title<h6>";?></h5>
+            
+            <button class="btn btn-light container" style="height: 70%;" type="button" data-bs-toggle="modal" data-bs-target="#UpTask<?php echo $id;?>">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" >
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="offcanvas-title " id="offcanvasLabel"><?php echo "Titulo: $title";?></h5>              
+                    </div>
+                    <div class="modal-body" >
+                        <?php echo "<p>Descricao: <br> $desc</p>";?>
+                    </div>
                 </div>
-                <div class="offcanvas-body">
-                <?php echo "<p>Descricao: <br> $desc</p>";?>
                 </div>
-            </div>
             </button>
-
-            <div class="modal fade" id="UpTask<?php echo $id;?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <form action="cadTask.php" method="POST">
+            <div class="modal fade" id="UpTask<?php echo $id;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <form action="index.php" method="POST">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="staticBackdropLabel">Nova Nota</h5>
@@ -221,13 +190,15 @@
                             </div>
                             <div class="modal-body">
                             <?php
-                                echo "<input type='text' placeholder='Titulo' name='title' value='$title'><br>";
-                                echo "<input type='hyder' placeholder='Titulo' name='idFunction' value='$userId'><br>";
-                                echo "<textarea name='desc' id='text' cols='20' rows='10'>$desc</textarea><br>";
-                                echo "<input type='checkbox' placeholder='Titulo' name='finalizada' "?>
-                                <?php if($dado['is_finished']){echo "checked";}?><?php echo">Finalizada ";
+                                echo "<input class='form-control' type='text' placeholder='Titulo' name='title' value='$title'><br>";
+                                echo "<input type='hidden' name='idFunction' value='2'><br>";
+                                echo "<input type='hidden' name='id' value='$userId'>";
+                                echo "<input type='hidden' name='pass' value='$userPass'>";
+                                echo "<input type='hidden' name='idTask' value='$id'>";
+                                echo "<textarea class='form-control' name='desc' cols='50' rows='10'>$desc</textarea><br>";
+                                echo "<input type='checkbox' name='isFin' "?>
+                                <?php if($dado['is_finished']){echo "checked";}?><?php echo">Finalizada <br>";
                             ?>
-
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
@@ -237,10 +208,17 @@
                     </form>
                 </div>
             </div>
-
-            <form action="upTask.php"></form>
+            <div >
+                <form class="row" action="index.php" method="POST">
+                    <?php
+                    echo "<input type='hidden' name='idFunction' value='3'><br>";
+                    echo "<input type='hidden' name='id' value='$userId'>";
+                    echo "<input type='hidden' name='pass' value='$userPass'>";
+                    echo "<input type='hidden' name='idTask' value='$id'>";?>
+                    <input type="submit" class="btn btn-primary mb-3" name="Apagar" value="Apagar"> 
+                </form>
             </div>
-            <?php //}  ?>
+            </div>
             <?php } ?>
             <?php }?>
   </div>
--- taskDAO.php
+++ taskDAO.php
@@ -1,33 +1,34 @@
 <?php
-
-$userId = $_POST['id'];
 //Funcoes da tabela Notas
-/*
-idFunction
-1-create
-2-update
-3-delete
-    */
+        /*
+        idFunction
+        1-create
+        2-update
+        3-delete
+         */
 if ($_POST['idFunction'] == 1) {
     $title = $_POST['title'];
     $desc = $_POST['desc'];
-    $query = "insert into note(title_,desc_,is_finished,user_id) value('{$title}','{$desc}',false,{$userId});";
+    $query = "insert into note(title_,desc_,is_finished,user_id) value('$title','$desc',false,$userId);";
     mysqli_query($mysqli, $query);
+    echo  "<script>alert('Adicionada com sucesso');</script>";
 
 }elseif ($_POST['idFunction'] == 2) {
+    
     $taskId = $_POST['idTask'];
     $title = $_POST['title'];
     $desc = $_POST['desc'];
-    $isFin = boolval($_POST['isFin']);    
+    $isFin = null;
+    if($_POST['isFin'] == "on"){$isFin = "true";}else{$isFin = "false";}
     $query = "update note set title_='$title',desc_='$desc',is_finished=$isFin where id_ = $taskId";
     mysqli_query($mysqli, $query);
+    echo  "<script>alert('Alterado com sucesso');</script>";
 
 }elseif($_POST['idFunction'] == 3) {
     $taskId = $_POST['idTask'];
     $query = "delete from note where id_ = $taskId";
     mysqli_query($mysqli, $query);
+    echo  "<script>alert('Apagado com sucesso');</script>";
 }
-
-header("locate: /.?id=$userId")
 
 ?>
